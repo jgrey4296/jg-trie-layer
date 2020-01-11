4e66e6a92cf56a20e5cd6101c0a22cee395c5566
John Grey <johngrey4296@gmail.com>
2020-01-11 06:11:19 +0000

    add more trie-layer variables

--- config.el
+++ config.el
@@ -3,6 +3,16 @@
 
 (defgroup trie-modes '() "Customization group for trie-related modes")
 
+(defvar jg-trie-layer/rule-helm-source nil
+  "Main Helm Source for rule loading / finding ")
+(defvar jg-trie-layer/rule-helm-dummy-source)
+(defvar jg-trie-layer/type-helm-source nil
+  "Main Helm Source for type loading / finding ")
+(defvar jg-trie-layer/type-helm-dummy-source)
+
+
+
+
 ;;Variables for the active ide
 (setq jg-trie-layer/trie-ide-is-running nil
       jg-trie-layer/python-process nil
@@ -11,6 +21,14 @@
       jg-trie-layer/ide-pipeline-spec-buffer nil
       )
 
+(defcustom jg-trie-layer/process-python-command
+  "python3"
+  "Name of the Python program to use")
+
+(defcustom jg-trie-layer/process-python-args
+  ""
+  "Arguments to python to run IDE server")
+
 
 ;;Defaults
 (setq-default
@@ -18,25 +36,26 @@
  jg-trie-layer/outputs-buffer-name "*Rule Outputs*"
  jg-trie-layer/working-group-buffer-name "*Rule Working Group*"
  jg-trie-layer/logging-buffer-name "*Rule Logs*"
+ jg-trie-layer/python-process-buffer-name "*Rule IDE*"
  jg-trie-layer/working-group-buffer-headings '("Defeaters"
-                                      "Interferers"
-                                      "Alternatives"
-                                      "Equal Depth"
-                                      "Relevant Types"
-                                      "Meta"
-                                      "Layer Stats"
-                                      "Tests")
+                                               "Interferers"
+                                               "Alternatives"
+                                               "Equal Depth"
+                                               "Relevant Types"
+                                               "Meta"
+                                               "Layer Stats"
+                                               "Tests")
  jg-trie-layer/data-loc-subdirs '("rules"
-                         "types"
-                         "crosscuts"
-                         "patterns"
-                         "tests")
+                                  "types"
+                                  "crosscuts"
+                                  "patterns"
+                                  "tests")
 
-)
+ )
 
 (spacemacs|define-transient-state trie-help-hydra
-      :title "Transient State for Help in Rule IDE"
-      :doc (concat "
+  :title "Transient State for Help in Rule IDE"
+  :doc (concat "
    | General           ^^|
    |-------------------^^+
    | [_q_] Quit          |
@@ -44,6 +63,6 @@
    |                   ^^|
    |                   ^^|
   ")
-      :bindings
-      ("q" nil :exit t)
-      )
+  :bindings
+  ("q" nil :exit t)
+  )
