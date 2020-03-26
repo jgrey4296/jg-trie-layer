3f407352747e1093027091915be175ce813ffd74
John Grey <johngrey4296@gmail.com>
2020-03-26 00:33:11 +0000

    fixup! add todos, adjust keybindings for layer movement

--- local/trie-face/trie-face.el
+++ local/trie-face/trie-face.el
@@ -1,5 +1,6 @@
 ;; Macros for generating faces programmatically for trie mode
 (require 'dash)
+(require 'cl)
 
 (defgroup trie-face-generator '() "Variables of the macro based face generation")
 (defcustom trie-defined-depth-count 10 "Number of Trie Faces to Generate" :type '(integer))
